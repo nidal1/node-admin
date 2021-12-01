910142092e9a9bb0146ac0993c7697407e79e931
nidal1 <touhami9@gmail.com>
2021-12-01 18:18:01 +0100

    Chart js modifications

--- src/js/app.js
+++ src/js/app.js
@@ -1,37 +1,69 @@
-
-const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart').getContext("2d");
+var gradientFill = ctx.createLinearGradient(500, 0, 100, 0);
+gradientFill.addColorStop(1, "rgba(55, 81, 255, 0.2)");
+gradientFill.addColorStop(0, "rgba(55, 81, 255, 0)");
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        FillTarget:"end",
         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0)'
-            ],
+            label: 'Nidal',
+            data: [22, 59, 37, 55, 50, 56],
+
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(55, 81, 255, 1)',
             ],
-            borderWidth: 1,
-            
+            fill: true,
+            backgroundColor: gradientFill,
+            borderWidth: 2,
+            pointBorderColor: "rgba(55, 81, 255, 1)",
+            pointHoverBackgroundColor: "#ffffff",
+            pointHoverBorderColor: "#3751ff",
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 4,
+            radius: 0,
+
+        },
+        {
+            label: 'Touhami',
+            data: [ 55, 22,50, 40, 37, 50],
+
+            borderColor: [
+                '#DFE0EB',
+            ],
+            borderWidth: 2,
+            pointBorderColor: "rgba(55, 81, 255, 1)",
+            pointHoverBackgroundColor: "#ffffff",
+            pointHoverBorderColor: "#3751ff",
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 4,
+            radius: 0,
+
         }]
     },
     options: {
-        tension:0.6,
+        responsive: false,
+        interaction: {
+            intersect: false
+        },
+        tension: 0.4,
+        layout: {
+            padding: 32
+        },
         filler: {
             propagate: false,
-          },
+        },
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                suggestedMin: 0,
+                suggestedMax: 60
+            },
+            x: {
+                display: false
             }
+
+
         }
     }
 });
